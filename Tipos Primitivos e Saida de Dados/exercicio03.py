000bba6790c54dd67a4b6b2d717b2202a17de08d
Gabs104 <107156801+Gabs104@users.noreply.github.com>
2023-02-12 13:31:56 -0300

    Update exercicio03.py

--- Tipos Primitivos e Saida de Dados/exercicio03.py
+++ Tipos Primitivos e Saida de Dados/exercicio03.py
@@ -1,6 +1,6 @@
 # Crie um programa que leia dois números e mostre a soma entre eles.
 trava = True
-while trava == True: # condição de loop até que o usupario tenha feito certo oq foi pedido
+while trava == True: # condição de loop até que o usuário tenha feito certo oq foi pedido
     num1 = input('digite um número: ')
     num2 = input('digite outro número: ')
     if num1.isnumeric() and num2.isnumeric() == True: # ira checar se os valores de num1 e num2 são numéricos
