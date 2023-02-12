fb10a5135da8e9305273afc458e1346789050260
Gabs104 <Gabrielrodriguescb9@gmail.com>
2023-02-12 13:13:21 -0300

    Primeiro Mundo

--- Tipos Primitivos e Saida de Dados/exercicio03.py
+++ Tipos Primitivos e Saida de Dados/exercicio03.py
@@ -3,7 +3,7 @@ trava = True
 while trava == True: # condição de loop até que o usupario tenha feito certo oq foi pedido
     num1 = input('digite um número: ')
     num2 = input('digite outro número: ')
-    if num1.isnumeric() or num2.isnumeric() == True: # ira checar se os valores de num1 e num2 são numéricos
+    if num1.isnumeric() and num2.isnumeric() == True: # ira checar se os valores de num1 e num2 são numéricos
         num1 = int(num1)
         num2 = int(num2)
         trava = False
