f36fe74c8976f9a61732289437ea5dbe9e556b28
Gabs104 <Gabrielrodriguescb9@gmail.com>
2023-02-12 23:14:48 -0300

    Primeiro Mundo

--- Utilizando Módulos/exercicio19e20.py
+++ Utilizando Módulos/exercicio19e20.py
@@ -5,8 +5,7 @@
 
 from random import choice,shuffle # o choice tem a função de escolher 1 elemento de uma lista
 alunos = ['Kamilla','Jordan','Renatão','João'] # criamos uma lista para usar no módulo choice que ira escolher entre os 4 elementos dados (uma lista não é necessária, poderiamos ter colocado as strings dentro do choice())
-aluno_esc = choice(alunos)
-alunos_ordem = shuffle(alunos)
+shuffle(alunos)
 print('Kamilla, Jordan, Renatão e João, um de vocês sera escolhido para apagar o quadro!')
-print(f'e esse aluno é você: {aluno_esc}! hahahaha!') # aluno escolhido é printado
+print(f'e esse aluno é você: {choice(alunos)}! hahahaha!') # aluno escolhido é printado
 print(f'e a ordem de apresentação do trabalho sera 1°:{alunos[0]}, 2°: {alunos[1]}, 3°: {alunos[2]}, 4°: {alunos[3]}')
